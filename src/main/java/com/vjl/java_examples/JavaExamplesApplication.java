21662e1d24196f3573cc1db3a833fa58cab4a46e
vjl1 <vjnjall@gmail.com>
2025-01-22 16:11:31 -0500

    added a starter project for java training

---
+++ src/main/java/com/vjl/java_examples/JavaExamplesApplication.java
@@ -0,0 +1,13 @@
+package com.vjl.java_examples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaExamplesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vjl/java_examples/JavaExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vjl.java_examples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaExamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
