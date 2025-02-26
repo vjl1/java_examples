c4ee59e50d5d8a7992d3a1cde82120c60a98ef15
vjl1 <vjnjall@gmail.com>
2025-02-26 00:10:45 -0500

    using yaml file for configuration and added hello world rest controller

---
+++ src/main/java/com/vjl/java_examples/spring/boot/hello/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.vjl.java_examples.spring.boot.hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+  @RequestMapping("/hello")
+  public String sayHello(){
+    return "Welcome to Hello World!!!";
+  }
+
+}
