5f98dc887a716c08f485f768d6d990e8462837cc
vjl1 <vijay.lingala@gmail.com>
2025-11-30 20:51:58 -0500

    new changes for Java 7

---
+++ src/main/java/com/vjl/java_examples/ver07/Literals.java
@@ -0,0 +1,28 @@
+package com.vjl.java_examples.ver07;
+
+public class Literals {
+
+    public void defineAndParseBeforeJava7(){
+        int binaryNumber = Integer.parseInt("1010", 2); // binary literal for 10
+        System.out.println("Binary 1010 as decimal: " + binaryNumber);
+        int million = 1000000; // no underscores
+        System.out.println("One million without underscores: " + million);     
+    }
+
+    public void defineAndParse(){
+        int binaryNumber = 0b1010; // binary literal for 10
+        System.out.println("Binary 0b1010 as decimal: " + binaryNumber);
+        int million = 1_000_000; // underscore for readability
+        System.out.println("One million with underscores: " + million);
+        int ipv4NetworkMask = 0b11111111_11111111_11111111_00000000; // IPv4 mask 255.255.255.0
+        System.out.println("IPv4 Network Mask: " + ipv4NetworkMask);
+    }
+    public static void main(String[] args) {
+        Literals literals = new Literals();
+        literals.defineAndParse();
+        System.out.println("--- Before Java 7 ---");
+        literals.defineAndParseBeforeJava7();
+    }
+}
+
+
---
+++ src/main/java/com/vjl/java_examples/ver07/StringsAsSwitch.java
@@ -0,0 +1,50 @@
+package com.vjl.java_examples.ver07;
+
+public class StringsAsSwitch {
+
+    public void printRoleMessageBeforeJava7(String role){
+        if (role == null){
+            System.out.println("no role");
+            return;
+        }
+        if(role.equals("admin")){
+            System.out.println("You have all the access");
+        } else if(role.equals("editor")){
+            System.out.println("You can edit content");
+        } else if(role.equals("viewer")){
+            System.out.println("You can view content");
+        } else {
+            System.out.println("No role assigned");
+        }
+    }
+    public void printRoleMessage(String role){
+        if (role == null){
+            System.out.println("no role");
+            return;
+        }
+        switch(role){
+            case "admin":
+                System.out.println("You have all the access");
+                break;
+            case "editor":
+                System.out.println("You can edit content");
+                break;
+            case "viewer":
+                System.out.println("You can view content");
+                break;
+            default:
+                System.out.println("No role assigned");
+        }
+    }
+
+    public static void main(String[] args) {
+        StringsAsSwitch example = new StringsAsSwitch();
+        example.printRoleMessage(null);
+        example.printRoleMessage("admin");
+        example.printRoleMessageBeforeJava7(null);
+        example.printRoleMessageBeforeJava7("admin");
+
+        
+    }
+
+}
