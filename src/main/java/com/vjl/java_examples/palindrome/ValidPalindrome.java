70676624c539e362413b08fad911d8bd63447bc4
vjl1 <vijay.lingala@gmail.com>
2025-12-06 10:03:12 -0500

    updted formatting for better readability

--- src/main/java/com/vjl/java_examples/palindrome/ValidPalindrome.java
+++ src/main/java/com/vjl/java_examples/palindrome/ValidPalindrome.java
@@ -3,33 +3,34 @@ package com.vjl.java_examples.palindrome;
 public class ValidPalindrome {
   public boolean isPalindrome(String input) {
     int start = 0;
-    int end = input.length()-1;
-    while(start<end){
-      if(input.charAt(start) == input.charAt(end)){
+    int end = input.length() - 1;
+    while (start < end) {
+      if (input.charAt(start) == input.charAt(end)) {
         start++;
         end--;
-      } else{
+      } else {
         return false;
       }
     }
     return true;
   }
 
-  /* option: using a string builder add all the letter or character and then use
-  builtin reverse method.
+  /*
+   * option: using a string builder add all the letter or character and then use builtin reverse
+   * method.
    */
   public boolean isPalindromeCompareCharAndDigit(String input) {
     int start = 0;
-    int end = input.length()-1;
-    while (start<end){
-      while(!Character.isLetterOrDigit(input.charAt(start)))
+    int end = input.length() - 1;
+    while (start < end) {
+      while (!Character.isLetterOrDigit(input.charAt(start)))
         start++;
-      while(!Character.isLetterOrDigit(input.charAt(end)))
+      while (!Character.isLetterOrDigit(input.charAt(end)))
         end--;
-      if(Character.toLowerCase(input.charAt(start)) == Character.toLowerCase(input.charAt(end))){
+      if (Character.toLowerCase(input.charAt(start)) == Character.toLowerCase(input.charAt(end))) {
         start++;
         end--;
-      }else{
+      } else {
         return false;
       }
     }
@@ -38,19 +39,23 @@ public class ValidPalindrome {
 
   public boolean isPalindromeCompareCharAndDigitWithOneSkip(String input) {
     int start = 0;
-    int end = input.length()-1;
+    int end = input.length() - 1;
     int skipCount = 0;
-    while(start<end){
-      while(!Character.isLetterOrDigit(input.charAt(start))) start++;
-      while(!Character.isLetterOrDigit(input.charAt(end))) end--;
-      if(Character.toLowerCase(input.charAt(start)) != Character.toLowerCase(input.charAt(end))){
-        if(skipCount<1){
+    while (start < end) {
+      while (!Character.isLetterOrDigit(input.charAt(start)))
+        start++;
+      while (!Character.isLetterOrDigit(input.charAt(end)))
+        end--;
+      if (Character.toLowerCase(input.charAt(start)) != Character.toLowerCase(input.charAt(end))) {
+        if (skipCount < 1) {
           skipCount++;
-          if(Character.toLowerCase(input.charAt(start+1)) == Character.toLowerCase(input.charAt(end))){
+          if (Character.toLowerCase(input.charAt(start + 1)) == Character
+              .toLowerCase(input.charAt(end))) {
             start++;
-          }else if(Character.toLowerCase(input.charAt(start)) == Character.toLowerCase(input.charAt(end-1))){
+          } else if (Character.toLowerCase(input.charAt(start)) == Character
+              .toLowerCase(input.charAt(end - 1))) {
             end--;
-          }else{
+          } else {
             return false;
           }
         }
--- src/main/java/com/vjl/java_examples/spring/boot/hello/HelloWorldController.java
+++ src/main/java/com/vjl/java_examples/spring/boot/hello/HelloWorldController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
 
   @RequestMapping("/hello")
-  public String sayHello(){
+  public String sayHello() {
     return "Welcome to Hello World!!!";
   }
 
